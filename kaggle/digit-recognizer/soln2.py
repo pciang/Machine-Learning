ac8365b90ee1761fee3bb4ab46993c2ab0d667c3
Peter <peterch1ang@yahoo.com>
2017-01-19 02:14:00 +0800

    Update

--- kaggle/digit-recognizer/soln2.py
+++ kaggle/digit-recognizer/soln2.py
@@ -2,29 +2,28 @@ import sys
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, Convolution2D, AveragePooling2D, Flatten, Activation, MaxPooling2D
+from keras.layers import Dense, Convolution2D, AveragePooling2D, Flatten, Activation, MaxPooling2D, Dropout
 from keras.optimizers import SGD
 from keras import backend as K
 
-def relu_init(shape, name=None):
-    value = np.random.normal(size=shape, loc=0.0, scale=0.1)
-    return K.variable(value)
-
 model = Sequential([
-    Convolution2D(30, 3, 3, border_mode='valid', init=relu_init, input_shape=(1,28,28), dim_ordering='th'),
+    Convolution2D(32, 5, 5, border_mode='valid', input_shape=(1,28,28), dim_ordering='th'),
     Activation('relu'),
     AveragePooling2D(pool_size=(2,2), strides=None, border_mode='valid', dim_ordering='th'),
-    Convolution2D(60, 4, 4, border_mode='valid', init=relu_init, input_shape=(1,28,28), dim_ordering='th'),
+    Dropout(0.25),
+    Convolution2D(64, 5, 5, border_mode='valid', input_shape=(1,28,28), dim_ordering='th'),
     Activation('relu'),
     AveragePooling2D(pool_size=(2,2), strides=None, border_mode='valid', dim_ordering='th'),
+    Dropout(0.25),
     Flatten(),
-    Dense(600, init=relu_init),
+    Dense(512),
     Activation('relu'),
+    Dropout(0.5),
     Dense(10),
     Activation('softmax'),
     ])
 
-sgd = SGD(lr=1e-2, momentum=0.8)
+sgd = SGD(lr=1e-1, momentum=0.7)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 data = pd.read_csv('train.csv', dtype=np.float32).as_matrix()
@@ -41,7 +40,7 @@ y_train = y_train.astype(np.float32)
 # Saving precious RAM?
 del data
 
-model.fit(X_train, y_train, batch_size=2000, nb_epoch=15)
+model.fit(X_train, y_train, batch_size=100, nb_epoch=3)
 
 ans = raw_input('continue? [y]/n: ')
 if ans != '' and ans != 'y' and ans != 'Y':
@@ -52,7 +51,7 @@ test_size = X_test.shape[0]
 X_test = (X_test - X_mean) / X_std
 X_test = np.reshape(X_test,(test_size,1,28,28))
 
-y_hat = model.predict_classes(X_test, batch_size=2000)[:,None]
+y_hat = model.predict_classes(X_test, batch_size=100)[:,None]
 img_id=np.arange(1,28001)[:,None]
 merged=np.concatenate((img_id,y_hat),axis=1)
 np.savetxt('submission2.csv', merged, delimiter=',',fmt='%d', header='ImageId,Label', comments='')
